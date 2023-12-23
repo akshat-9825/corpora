66bc415ddb401c85b2bb850d672c7e054ce23fef
Akshat Dwivedi <56993884+akshat-9825@users.noreply.github.com>
2023-12-23 20:16:34 +0530

    Client base code (#2)
    * added tailwind css, sass for client
    
    * created the left bar
    
    * removed tailwind postcss and autoprefixer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
+import Layout from "./layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+        
 import "./App.scss";
 
 function App() {
-  return <div className="text-3xl font-bold underline">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<>Hello</>} />
+          <Route path="/profile" element={<>profile</>} />
+          <Route path="/search" element={<>search</>} />
+          <Route path="/trending" element={<>trending</>} />
+          <Route path="/create" element={<>create</>} />
+          <Route path="/*" element={<>Not available</>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
