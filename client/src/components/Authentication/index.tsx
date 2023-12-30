2df1c6a4d154510dce4a5c33b7cddbf933a06699
Akshat Dwivedi <56993884+akshat-9825@users.noreply.github.com>
2023-12-30 13:27:05 +0530

    Client authentication (#3)
    * initial commit
    
    * finishing up authentication frontend

---
+++ client/src/components/Authentication/index.tsx
@@ -0,0 +1,178 @@
+import { Box, Button, TextField, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+
+interface AuthenticationType {
+  type: "login" | "signup";
+}
+
+const Authentication = ({ type }: AuthenticationType) => {
+  const theme = useTheme();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        sx={{
+          my: 8,
+          mx: 4,
+          display: "flex",
+          flexDirection: "column",
+          width: "fit-contents",
+        }}>
+        <Box
+          sx={{
+            alignSelf: "flex-start",
+            [theme.breakpoints.down("tablet")]: {
+              alignSelf: "center",
+              textAlign: "center",
+            },
+          }}>
+          <Typography
+            sx={{
+              fontSize: "48px",
+              color: "white",
+              fontFamily: "DM Serif Display",
+            }}>
+            Welcome
+          </Typography>
+          <Typography
+            sx={{
+              color: "#A5A5A5",
+              fontFamily: "Lexend Deca",
+              fontSize: "24px",
+              fontWeight: "300",
+            }}>
+            {type === "login"
+              ? "Let's log you in quickly"
+              : "Let's sign you up quickly"}
+          </Typography>
+        </Box>
+        <Box
+          component="form"
+          noValidate
+          onSubmit={handleSubmit}
+          sx={{ mt: "37px" }}>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="email"
+            label="Enter your email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            sx={{
+              my: 0,
+            }}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            name="password"
+            label="Enter your password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            sx={{
+              my: "22px",
+            }}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              gap: 4,
+              [theme.breakpoints.down("tablet")]: {
+                flexDirection: "column",
+              },
+            }}>
+            <Box>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  width: "180px",
+                  height: "60px",
+                  ":hover": {
+                    bgcolor: theme.palette.primary.main,
+                  },
+                  borderRadius: 0,
+                  fontWeight: "600",
+                  [theme.breakpoints.down("tablet")]: {
+                    height: "52px",
+                    width: "100%",
+                  },
+                }}>
+                {type === "login" ? "LOGIN" : "SUBMIT"}
+              </Button>
+            </Box>
+            <Box>
+              {type === "login" ? (
+                <Typography
+                  sx={{
+                    color: "#FFF",
+                    fontFamily: "Lexend Deca",
+                    fontSize: "20px",
+                    fontWeight: "400",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}>
+                  don't have an account?
+                  <Button
+                    variant="text"
+                    sx={{
+                      color: "#6EEB83",
+                      fontFamily: "Lexend Deca",
+                      fontSize: "20px",
+                      fontWeight: "400",
+                      textTransform: "none",
+                      p: 0,
+                      justifyContent: "flex-start",
+                      width: "fit-content",
+                    }}>
+                    <Link to="/signup">sign-up</Link>
+                  </Button>
+                </Typography>
+              ) : (
+                <Typography
+                  sx={{
+                    color: "#FFF",
+                    fontFamily: "Lexend Deca",
+                    fontSize: "20px",
+                    fontWeight: "400",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}>
+                  already have an account?
+                  <Button
+                    variant="text"
+                    sx={{
+                      color: "#6EEB83",
+                      fontFamily: "Lexend Deca",
+                      fontSize: "20px",
+                      fontWeight: "400",
+                      textTransform: "none",
+                      p: 0,
+                      justifyContent: "flex-start",
+                      width: "fit-content",
+                    }}>
+                    <Link to="/login">sign-in</Link>
+                  </Button>
+                </Typography>
+              )}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Authentication;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import CorporaRoutes from "./CorporaRoutes.tsx";
 
 import ScopedCssBaseline from "@mui/material/ScopedCssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material";
 import "./index.scss";
+
+declare module "@mui/material/styles" {
+  interface BreakpointOverrides {
+    xs: false; // removes the `xs` breakpoint
+    sm: false;
+    md: false;
+    lg: false;
+    xl: false;
+    mobile: true; // adds the `mobile` breakpoint
+    tablet: true;
+    laptop: true;
+    desktop: true;
+    largeDesktop: true;
+  }
+}
 
 const darkTheme = createTheme({
   palette: {
@@ -15,13 +30,22 @@ const darkTheme = createTheme({
       dark: "#000",
     },
   },
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 768,
+      laptop: 1024,
+      desktop: 1200,
+      largeDesktop: 1440,
+    },
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ScopedCssBaseline />
     <ThemeProvider theme={darkTheme}>
-      <App />
+      <CorporaRoutes />
     </ThemeProvider>
   </React.StrictMode>
 );
