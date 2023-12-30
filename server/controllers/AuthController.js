999cc65444976d538b8eb96bb489376d9dde08d3
Akshat-Dwivedi <akshat10dwivedi@gmail.com>
2023-12-30 19:33:45 +0530

    implemented jwt authentication

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,62 @@
+const Users = require("../models/Users");
+const { createSecretToken } = require("../utils/SecretToken");
+const bcrypt = require("bcryptjs");
+
+module.exports.Signup = async (req, res, next) => {
+  try {
+    const { email, phone, password, username, createdAt } = req.body;
+    const existingUser = await Users.findOne({ email });
+    if (existingUser) {
+      return res.json({ message: "User already exists" });
+    }
+    const user = await Users.create({
+      email,
+      phone,
+      password,
+      username,
+      createdAt,
+    });
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User signed in successfully", success: true, user });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports.Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!(email || phone)) {
+      return res.json({ message: "Email or Phone required" });
+    }
+    if (!password) {
+      return res.json({ message: "Password required" });
+    }
+    const user = await Users.findOne({ email });
+    if (!user) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
--- server/views/server.js
+++ server/views/server.js
@@ -1,25 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const app = express();
-require("dotenv").config();
-const port = 5001;
-
-const apiRoute = require("../routes/user-routes");
+const { PORT, MONGO_URI: connectionString } = process.env;
 
 //MiddleWares
 app.use(express.json());
-app.use(cors());
-
-app.use("/api", apiRoute);
-
-const connectionString = process.env.MONGO_URI;
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 mongoose
   .connect(connectionString)
   .then(() => console.log("Connected to the database…"))
   .catch((err) => console.error("Connection error:", err));
 
-app.listen(port, () => {
-  console.log(`Server is listening on ${port} port....`);
+const authRoute = require("../routes/AuthRoutes");
+app.use("/api", authRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT} port....`);
 });
